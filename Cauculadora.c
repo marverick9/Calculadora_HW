14f6d0b2a97d08e14f25340013794a92522276f7
marverick9 <134337164+marverick9@users.noreply.github.com>
2024-08-30 14:42:30 -0300

    Add files via upload
    Questão 1-)

---
+++ Cauculadora.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int conversor(int numero){
+    int converte[8];
+    int n =numero;
+    while(n>0){
+        if(n%2==0){
+            converter+=0;
+        }
+        
+
+    }
+
+    return 0;
+}
+int main(void){
+    int numero;
+    scanf("%d",&numero);
+    
+}
